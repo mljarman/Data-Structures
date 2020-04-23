ab7a95240665f26f6f77ce93ffde940a05315394
mljarman <maggiejarman2@gmail.com>
2020-04-23 09:30:41 -0600

    day 3 MVP

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -12,16 +12,51 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        # self.left and/or self.right need to be valid nodes
+        # for us to call 'insert' on them
+        if value < self.value:
+            # check if self.left is a valid node
+            if self.left:
+                self.left.insert(value)
+                # the left side is empty
+            else:
+                # we've found parking spot
+                self.left = BinarySearchTree(value)
+        # otherwise, value >= self.value
+        else: # value is greater than or equal to
+            if self.right:
+                self.right.insert(value)
+            else:
+                self.right = BinarySearchTree(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        # base case: if value is target, return True
+        if self.value == target:
+            return True
+        if target < self.value:
+            if self.left:
+                return self.left.contains(target)
+            else:
+                return False
+        # if bigger, but we can't go right, return False
+        else:
+            if self.right:
+                return self.right.contains(target)
+            else:
+                return False
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        # will be furthest node to right.
+        # base case: no more nodes to right.
+        if self.right == None:
+            return None
+        else:
+            self.right.get_max()
+            return self.value
+
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
